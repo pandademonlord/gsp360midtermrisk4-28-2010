dd050082dbd7ede34d6610db75fa2425004716a0
mknoche <mknoche@01c8a8da-d48b-78e0-b892-dcd36b50181e>
2010-04-28 23:46:03 +0000

    Empty glut window

---
+++ riskyBsns/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "glut.h"
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+void main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(1230,630);
+	glutCreateWindow("Risk");
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
