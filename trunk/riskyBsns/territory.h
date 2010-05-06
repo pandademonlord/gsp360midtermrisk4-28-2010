cea4e84b57fe0007ea79802e80a9ad9981ac5e13
jparks.gsp <jparks.gsp@01c8a8da-d48b-78e0-b892-dcd36b50181e>
2010-05-06 03:29:58 +0000

    -added/modified the values of the territory IDs (still the same values, but they're now based on an algorithm) -ALL connections are displayed in grey color -each territory's circle is displayed in diff color, like the map on Wikipedia (N.Amer = yellow, S.Amer = red, Europe = blue, Africa = orange, Asia = green, Australia/Oceania = purple) -chgd the "highlighted/hovering" color to white

--- trunk/riskyBsns/territory.h
+++ trunk/riskyBsns/territory.h
@@ -7,25 +7,42 @@ class Territory
 {
 private:
 	Circle m_area;
+	short m_ID;
 	short m_continentID;
 	TemplateArray<Territory *> m_connect;
 	short m_ownerID;
 	short m_troops_deployed;
 public:
-	Territory(short a_continentID)
+	Territory(short a_ID, short a_continentID)
 	{
 		m_area = Circle();
+		m_ID = a_ID;
 		m_continentID = a_continentID;
 		m_ownerID = OWNER_NONE;
 		m_troops_deployed = 0;
+		switch(this->m_continentID)
+		{
+		case CONTINENT_ID_N_AMERICA:
+			this->setColor(COLOR_YELLOW);
+			break;
+		case CONTINENT_ID_S_AMERICA:
+			this->setColor(COLOR_RED);
+			break;
+		case CONTINENT_ID_EUROPE:
+			this->setColor(COLOR_BLUE);
+			break;
+		case CONTINENT_ID_AFRICA:
+			this->setColor(COLOR_ORANGE);
+			break;
+		case CONTINENT_ID_ASIA:
+			this->setColor(COLOR_GREEN);
+			break;
+		case CONTINENT_ID_OCEANIA:
+			this->setColor(COLOR_PURPLE);
+			break;
+		}
 	}
-	Territory(V2DF a_pos, float a_radius, short a_continentID)
-	{
-		m_area = Circle(a_pos, a_radius);
-		m_continentID = a_continentID;
-		m_ownerID = OWNER_NONE;
-		m_troops_deployed = 0;
-	}
+	//sets the clickable size of the territory
 	void setArea(V2DF a_pos, float a_radius)
 	{
 		m_area.setCenter(a_pos);
@@ -50,10 +67,44 @@ public:
 		else if(m_troops_deployed < 1)
 			m_troops_deployed = 1;
 	}
+	//returns the territory's position as a V2D
+	V2DF getPosition() {return this->m_area.getCenter();}
+	//returns the territory's unique ID
+	short getID(){return this->m_ID;}
+	//returns the continent to which the territory belongs to
+	short getContinent(){return m_continentID;}
+	//returns the owner's ID
+	short getOwner(){return m_ownerID;}
+	//returns how many troops are currently deployed within the territory
+	short getTroops(){return m_troops_deployed;}
 	//sets the color to display the area
 	void setColor(short a_r, short a_g, short a_b)
 	{
 		this->m_area.setColor(a_r, a_g, a_b);
+	}
+	void useContinentColor()
+	{
+		switch(this->m_continentID)
+		{
+		case CONTINENT_ID_N_AMERICA:
+			this->setColor(COLOR_YELLOW);
+			break;
+		case CONTINENT_ID_S_AMERICA:
+			this->setColor(COLOR_RED);
+			break;
+		case CONTINENT_ID_EUROPE:
+			this->setColor(COLOR_BLUE);
+			break;
+		case CONTINENT_ID_AFRICA:
+			this->setColor(COLOR_ORANGE);
+			break;
+		case CONTINENT_ID_ASIA:
+			this->setColor(COLOR_GREEN);
+			break;
+		case CONTINENT_ID_OCEANIA:
+			this->setColor(COLOR_PURPLE);
+			break;
+		}
 	}
 	//returns the # of existing connections
 	short getNumberConnections(){return this->m_connect.size();}
@@ -67,17 +118,10 @@ public:
 		}
 		return false;
 	}
-	//returns the territory's position as a V2D
-	V2DF getPosition() {return this->m_area.getCenter();}
-	//returns the continent to which the territory belongs to
-	short getContinent(){return m_continentID;}
-	//returns the owner's ID
-	short getOwner(){return m_ownerID;}
-	//returns how many troops are currently deployed within the territory
-	short getTroops(){return m_troops_deployed;}
 	void glDraw()
 	{
 		m_area.glDraw();
+		glColor3f(COLOR_GREY);
 		for(int i = 0; i < this->m_connect.size(); ++i)
 		{
 			glBegin(GL_LINES);
