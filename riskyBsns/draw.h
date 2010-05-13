1dd065bb75cc01506c1130c79d255156462ddf75
jparks.gsp <jparks.gsp@01c8a8da-d48b-78e0-b892-dcd36b50181e>
2010-05-13 19:24:43 +0000

    -added UI directions of what you can press/click, and what that'll do

--- riskyBsns/draw.h
+++ riskyBsns/draw.h
@@ -43,6 +43,7 @@ void display()
 				break;
 			case STATE_FORTIFY_TO:
 				sprintf(buffer, "Player %d's turn: Fortify (Where to Send Troops?)\n", (players.get(i)->getID() + 1));
+				(V2DF(0,5)).glDrawString(buffer);
 				break;
 			case STATE_FORTIFY_TROOPS:
 				sprintf(buffer, "Player %d's turn: Fortify (Moving %d Troops)\n", (players.get(i)->getID() + 1), flags[FLAG_PARAM_NUM]);
@@ -51,7 +52,35 @@ void display()
 				sprintf(buffer, "Player %d WINS!\n", (players.get(i)->getID() + 1), players.get(i)->getTroops());
 				break;
 			}
-			(V2DF(0,0)).glDrawString(buffer);
+			(V2DF(0,5)).glDrawString(buffer);
+
+			switch(flags[FLAG_GAME_STATE])
+			{
+			case STATE_INIT_PLACEMENT_CLAIM:
+				sprintf(buffer, "Left-Click on any unoccupied territory.\n");
+				break;
+			case STATE_INIT_PLACEMENT_PLACE:
+			case STATE_PLACE_BONUS_TROOPS:
+			case STATE_PLACE_EXCESS_TROOPS:
+				sprintf(buffer, "Left-Click on a territory you occupy.\n");
+				break;
+			case STATE_ATTACK_FROM:
+				sprintf(buffer, "Left-Click on a territory you occupy (2+ troops), OR Left-Click outside a territory to end attack.\n");
+				break;
+			case STATE_ATTACK_TO:
+				sprintf(buffer, "Left-Click on an enemy territory adjacent to your territory, OR Right-Click to go back to Prev state.\n");
+				break;
+			case STATE_FORTIFY_FROM:
+				sprintf(buffer, "Left-Click on a territory you occupy (2+ troops), OR Left-Click outside a territory to end turn.\n");
+				break;
+			case STATE_FORTIFY_TO:
+				sprintf(buffer, "Left-Click on a territory you occupy that's adjacent to your territory, OR Right-Click to go back to Prev state.\n");
+				break;
+			case STATE_FORTIFY_TROOPS:
+				sprintf(buffer, "Press W/S to increase/decrease # of troops to move (Space to finish move).\n");
+				break;
+			}
+			(V2DF(0,20)).glDrawString(buffer);
 		}
 	}
 
